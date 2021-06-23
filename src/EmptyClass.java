a6e28cba76cc1837486f6317c469274178c6c0d1
Qa-Automation30 <qaautomation30@gmail.com>
2021-06-23 11:54:26 +0530

    add new feature

--- src/EmptyClass.java
+++ src/EmptyClass.java
@@ -3,6 +3,15 @@ public class EmptyClass {
 
 	public static void main(String[] args) {
 		// This is just for example to check git commands
+		
+		// add more comments as need to check branch status
+		
+		// add more comments as need to check branch status-1
+		// add more comments as need to check branch status-2
+		// add more comments as need to check branch status-3
+		//// add more comments as need to check branch status-4
+		//// add more comments as need to check branch status-5
+		//// add more comments as need to check branch status-6
 
 	}
 
